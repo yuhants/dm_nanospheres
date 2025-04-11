5ebeb1f9fa6e5d5c2db8c6e4f01ea6938ae080ef
unknown <yuhantseng.09@gmail.coom>
2025-04-11 19:15:08 -0400

    new calibration data

--- process_calibration_data.py
+++ process_calibration_data.py
@@ -10,10 +10,11 @@ out_dir = r'C:\Users\yuhan\dm_nanospheres\data_processed\sphere_data'
 # out_dir = '/Users/yuhan/work/nanospheres/data/pulse_calibration_processed'
 
 sphere = 'sphere_20250406'
-datasets = ['20250410_m31e_alignment1_3e-8mbar_trapping_4', '20250410_m31e_alignment1_3e-8mbar_trapping_5', '20250410_m31e_alignment1_3e-8mbar_trapping_6', '20250410_m31e_alignment1_3e-8mbar_trapping_7']
-dataset_prefixs = ['20250410_dg_m31e_200ns_', '20250410_dg_m31e_200ns_', '20250410_dg_m31e_200ns_', '20250410_dg_m31e_200ns_']
-# datasets = ['20250408_m28e_alignment0_4e-8mbar_trapping_0', '20250408_m28e_alignment0_4e-8mbar_trapping_1', '20250408_m28e_alignment0_4e-8mbar_trapping_2']
-# dataset_prefixs = ['20250408_dg_m28e_200ns_', '20250408_dg_m28e_200ns_', '20250408_dg_m28e_200ns_']
+datasets = ['20250411_m8e_alignment1_2e-8mbar_trapping_0', '20250411_m8e_alignment1_2e-8mbar_trapping_1', '20250411_m8e_alignment1_2e-8mbar_trapping_2', '20250411_m8e_alignment1_2e-8mbar_trapping_3']
+dataset_prefixs = ['20250411_dg_m8e_200ns_', '20250411_dg_m8e_200ns_', '20250411_dg_m8e_200ns_', '20250411_dg_m8e_200ns_']
+# datasets = ['20250410_m31e_alignment1_3e-8mbar_trapping_4', '20250410_m31e_alignment1_3e-8mbar_trapping_5', '20250410_m31e_alignment1_3e-8mbar_trapping_6', '20250410_m31e_alignment1_3e-8mbar_trapping_7']
+# dataset_prefixs = ['20250410_dg_m31e_200ns_', '20250410_dg_m31e_200ns_', '20250410_dg_m31e_200ns_', '20250410_dg_m31e_200ns_']
+
 
 # sphere = 'sphere_20250103'
 # For Sphere 20250103, exclude two calibration datasets (20240109*)
@@ -89,7 +90,7 @@ for i in range(len(voltages)):
 unnormalized_amps_noise_flattened = np.concatenate(unnormalized_amps_noise)
 unnormalized_amps_noise_nosearch_flattened = np.concatenate(unnormalized_amps_noise_nosearch)
 
-outfile_name = f'{sphere}_calibration_unnormalized_amps_20250410_4_7.h5py'
+outfile_name = f'{sphere}_calibration_unnormalized_amps_20250411_3.h5py'
 print(f'Writing file {outfile_name}')
 with h5py.File(os.path.join(out_dir, outfile_name), 'w') as fout:
     g = fout.create_group('processed_amplitudes')
