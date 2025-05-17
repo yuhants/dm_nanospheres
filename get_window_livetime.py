cf73de6d5c6b2c5daa2823b8bfcc7291405accf9
Yu-Han Tseng <yuhan@Yu-Hans-Air.hsd1.ct.comcast.net>
2025-05-17 13:29:52 -0400

    minor update

--- get_window_livetime.py
+++ get_window_livetime.py
@@ -2,6 +2,9 @@ import numpy as np
 import h5py
 import os
 
+from astropy.time import Time, TimeDelta
+from astropy.coordinates import EarthLocation
+
 noise_thr = 200
 dt_window = 10e-3
 
@@ -94,6 +97,86 @@ n_files = [1440, 1440, 1440, 1440, 1440, 821, 640, 181, 1440, 1418, 917, 1169, 1
 #                 '20250125_d_',
 #                 ]
 # n_files = [1440, 900, 1440, 1440, 1440, 1440, 1440, 1440, 780, 1440, 1440, 1440, 1440, 1440, 1983, 1463, 1440, 1440, 1440, 1440, 1121]
+
+latitude_newhaven  = 41.31 # deg
+longitude_newhaven = -72.923611 # deg
+elevation_newhaven = 18 # m
+
+location = EarthLocation(lat=latitude_newhaven, lon=longitude_newhaven, height=elevation_newhaven)
+
+def get_local_hour(utc_offset, t_utc):
+    t_locals = t_utc + TimeDelta(utc_offset * 3600, format='sec')
+
+    t_local_hours = np.empty(t_locals.size)
+    for i, t_local in enumerate(t_locals):
+        t_local_hours[i] = t_local.datetime.hour + t_local.datetime.minute/60 + t_local.datetime.second/3600
+
+    return t_local_hours
+
+def group_into_days(hours):
+    idx_day = np.where(np.diff(hours)<0)[0]
+
+    hours_day = []
+    for i, idx_end in enumerate(idx_day):
+        if i == 0:
+            idx_start = 0
+        else:
+            idx_start = idx_day[i-1]
+        hours_day.append(hours[idx_start+1:idx_end+1])
+        
+    # Add the final day
+    hours_day.append(hours[idx_end+1:])
+    
+    return hours_day
+
+def get_hist_days(sphere, datasets, bins, sidereal=True):
+    hist_days = []
+    hours_tmp = None
+    
+    for dataset in datasets:
+        npz = np.load(fr"E:\livetime_window\{sphere}\{sphere}_{dataset}_livetime_window.npz")
+        time_window = npz['livetime_window']
+        npz.close()
+
+        tt = Time(time_window, format='unix', scale='utc')
+        if sidereal:
+            lst = tt.sidereal_time('apparent', longitude=location.lon)
+            hours = lst.hour
+        else:
+            utc_offset = -5
+            hours = get_local_hour(utc_offset, tt)
+
+        # Compute histogram until the end of sidereal day
+        next_day = False
+        _idx_day = np.where(np.diff(hours) < 0)[0]
+        if _idx_day.size == 1:
+            idx_day = _idx_day[0]
+            next_day = True
+        else:
+            idx_day = hours.size
+
+        _hist, _ = np.histogram(hours[:idx_day+1], bins=bins)
+
+        # If there is left over hours from previous dataset, add them
+        # to the histogram
+        if hours_tmp is not None:
+            _hist_prev, _ = np.histogram(hours_tmp, bins=bins)
+            _hist += _hist_prev
+
+        hist_days.append(_hist)
+
+        if next_day:
+            hours_tmp = hours[idx_day+1:]
+        else:
+            hours_tmp = None
+
+    # Add the final bit if there is leftover
+    if hours_tmp is not None:
+        _hist_leftover, _ = np.histogram(hours_tmp, bins=bins)
+        hist_days.append(_hist_leftover)
+
+    return np.asarray(hist_days)
+    
 
 def get_livetime_window(sphere, dataset, data_prefix, nfile):
     data_dir = fr'E:\dm_data_processed_amp_chisquare\{sphere}\{dataset}'
