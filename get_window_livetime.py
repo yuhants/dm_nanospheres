e3f7d090b19b461bb56d514d42c370965e6becbb
unknown <yuhantseng.09@gmail.coom>
2025-05-18 22:38:13 -0400

    update livetime window

--- get_window_livetime.py
+++ get_window_livetime.py
@@ -1,6 +1,6 @@
 import numpy as np
 import h5py
-import os
+import os, sys
 
 from astropy.time import Time, TimeDelta
 from astropy.coordinates import EarthLocation
@@ -11,26 +11,26 @@ dt_window = 10e-3
 amp2kev, sigma_p = 5945.245097647231, 148.3093981742833   # Sphere 20241202
 # amp2kev, sigma_p = 5923.2059527417405, 146.88560114966003 # Sphere 20250103
 
-sphere = 'sphere_20241202'
-datasets = [
-            '20241202_8e-8mbar_long',
-            '20241204_2e-8mbar_8e_aftercal_long',
-            '20241205_2e-8mbar_0e_aftercal_long',
-            '20241206_1e-8mbar_0e_aftercal_long',
-            '20241207_1e-8mbar_1e_aftercal_long',
-            '20241208_1e-8mbar_1e_aftercal_long',
-            '20241210_1e-8mbar_8e_alignment1_long',
-            '20241210_1e-8mbar_8e_alignment2_long_withdrive',
-            '20241210_1e-8mbar_8e_alignment2_long_nodrive',
-            '20241211_1e-8mbar_8e_alignment2_long_nodrive',
-            '20241212_1e-8mbar_8e_alignment2_long_nodrive',
-            '20241213_1e-8mbar_0e_alignment2_long',
-            '20241214_1e-8mbar_0e_alignment2_long',
-            '20241215_9e-9mbar_0e_alignment2_long',
-            '20241216_5e-8mbar_0e_alignment2_long',
+sphere_0 = 'sphere_20241202'
+datasets_0 = [
+            # '20241202_8e-8mbar_long',
+            # '20241204_2e-8mbar_8e_aftercal_long',
+            # '20241205_2e-8mbar_0e_aftercal_long',
+            # '20241206_1e-8mbar_0e_aftercal_long',
+            # '20241207_1e-8mbar_1e_aftercal_long',
+            # '20241208_1e-8mbar_1e_aftercal_long',
+            # '20241210_1e-8mbar_8e_alignment1_long',
+            # '20241210_1e-8mbar_8e_alignment2_long_withdrive',
+            # '20241210_1e-8mbar_8e_alignment2_long_nodrive',
+            # '20241211_1e-8mbar_8e_alignment2_long_nodrive',
+            # '20241212_1e-8mbar_8e_alignment2_long_nodrive',
+            # '20241213_1e-8mbar_0e_alignment2_long',
+            # '20241214_1e-8mbar_0e_alignment2_long',
+            # '20241215_9e-9mbar_0e_alignment2_long',
+            # '20241216_5e-8mbar_0e_alignment2_long',
             '20241217_6e-8mbar_0e_alignment3_long',
             ]
-data_prefixs = [
+data_prefixs_0 = [
                 '20241202_abcd_',
                 '20241204_abcd_',
                 '20241205_d_',
@@ -48,55 +48,55 @@ data_prefixs = [
                 '20241216_d_',
                 '20241217_d_',
                 ]
-n_files = [1440, 1440, 1440, 1440, 1440, 821, 640, 181, 1440, 1418, 917, 1169, 1565, 1440, 1164, 601]
+n_files_0 = [1440, 1440, 1440, 1440, 1440, 821, 640, 181, 1440, 1418, 917, 1169, 1565, 1440, 1164, 601]
 
-# sphere = 'sphere_20250103'
-# datasets = ['20250104_4e-8mbar_alignment0_long',
-#             '20250105_2e-8mbar_alignment0_long',
-#             '20250106_2e-8mbar_8e_alignment0_long',
-#             '20250107_1e-8mbar_8e_alignment0_long',
-#             '20250108_1e-8mbar_8e_alignment0_long',
-#             '20250109_1e-8mbar_8e_alignment1_long',
-#             '20250110_1e-8mbar_8e_alignment1_long',
-#             '20250111_1e-8mbar_8e_alignment1_long',
-#             '20250112_9e-9mbar_8e_alignment1_long',
-#             '20250113_5e-8mbar_8e_alignment1_long',
-#             '20250114_1e-8mbar_1e_alignment1_long',
-#             '20250115_8e-9mbar_0e_alignment1_long',
-#             '20250116_8e-9mbar_0e_alignment1_long_wrong_lo',
-#             '20250117_8e-9mbar_0e_alignment1_long',
-#             '20250118_8e-9mbar_1e_alignment1_long',
-#             '20250120_8e-9mbar_1e_alignment1_long_wbackscat',
-#             '20250121_8e-9mbar_1e_alignment1_long',
-#             '20250122_8e-9mbar_1e_alignment1_long',
-#             '20250123_7e-9mbar_1e_alignment1_long',
-#             '20250124_7e-9mbar_1e_alignment1_long',
-#             '20250125_7e-9mbar_1e_alignment1_long'    
-#            ]
+sphere_1 = 'sphere_20250103'
+datasets_1 = [#'20250104_4e-8mbar_alignment0_long',
+            # '20250105_2e-8mbar_alignment0_long',
+            # '20250106_2e-8mbar_8e_alignment0_long',
+            # '20250107_1e-8mbar_8e_alignment0_long',
+            # '20250108_1e-8mbar_8e_alignment0_long',
+            # '20250109_1e-8mbar_8e_alignment1_long',
+            # '20250110_1e-8mbar_8e_alignment1_long',
+            # '20250111_1e-8mbar_8e_alignment1_long',
+            # '20250112_9e-9mbar_8e_alignment1_long',
+            # '20250113_5e-8mbar_8e_alignment1_long',
+            # '20250114_1e-8mbar_1e_alignment1_long',
+            # '20250115_8e-9mbar_0e_alignment1_long',
+            # '20250116_8e-9mbar_0e_alignment1_long_wrong_lo',
+            # '20250117_8e-9mbar_0e_alignment1_long',
+            '20250118_8e-9mbar_1e_alignment1_long',
+            # '20250120_8e-9mbar_1e_alignment1_long_wbackscat',
+            # '20250121_8e-9mbar_1e_alignment1_long',
+            # '20250122_8e-9mbar_1e_alignment1_long',
+            # '20250123_7e-9mbar_1e_alignment1_long',
+            # '20250124_7e-9mbar_1e_alignment1_long',
+            # '20250125_7e-9mbar_1e_alignment1_long'    
+           ]
 
-# data_prefixs = ['20250104_d_',
-#                 '20250105_d_',
-#                 '20250106_d_',
-#                 '20250107_d_',
-#                 '20250108_d_',
-#                 '20250109_d_',
-#                 '20250110_d_',
-#                 '20250111_d_',
-#                 '20250112_d_',
-#                 '20250113_d_',
-#                 '20250114_d_',
-#                 '20250115_d_',
-#                 '20250116_d_',
-#                 '20250117_d_',
-#                 '20250118_d_',
-#                 '20250120_d_',
-#                 '20250121_d_',
-#                 '20250122_d_',
-#                 '20250123_d_',
-#                 '20250124_d_',
-#                 '20250125_d_',
-#                 ]
-# n_files = [1440, 900, 1440, 1440, 1440, 1440, 1440, 1440, 780, 1440, 1440, 1440, 1440, 1440, 1983, 1463, 1440, 1440, 1440, 1440, 1121]
+data_prefixs_1 = ['20250104_d_',
+                '20250105_d_',
+                '20250106_d_',
+                '20250107_d_',
+                '20250108_d_',
+                '20250109_d_',
+                '20250110_d_',
+                '20250111_d_',
+                '20250112_d_',
+                '20250113_d_',
+                '20250114_d_',
+                '20250115_d_',
+                '20250116_d_',
+                '20250117_d_',
+                '20250118_d_',
+                '20250120_d_',
+                '20250121_d_',
+                '20250122_d_',
+                '20250123_d_',
+                '20250124_d_',
+                '20250125_d_',
+                ]
+n_files_1 = [1440, 900, 1440, 1440, 1440, 1440, 1440, 1440, 780, 1440, 1440, 1440, 1440, 1440, 1983, 1463, 1440, 1440, 1440, 1440, 1121]
 
 latitude_newhaven  = 41.31 # deg
 longitude_newhaven = -72.923611 # deg
@@ -131,7 +131,7 @@ def group_into_days(hours):
 
 def get_hist_days(sphere, datasets, bins, sidereal=True):
     hist_days = []
-    hours_tmp = None
+    _hours_tmp = None
     
     for dataset in datasets:
         npz = np.load(fr"E:\livetime_window\{sphere}\{sphere}_{dataset}_livetime_window.npz")
@@ -146,33 +146,39 @@ def get_hist_days(sphere, datasets, bins, sidereal=True):
             utc_offset = -5
             hours = get_local_hour(utc_offset, tt)
 
-        # Compute histogram until the end of sidereal day
-        next_day = False
+        # Compute histogram until the end of day
         _idx_day = np.where(np.diff(hours) < 0)[0]
+
+        # If there is an end of day in the dataset
+        # Bin into histogram until the end of day
+        # and store the rest as tmp
         if _idx_day.size == 1:
             idx_day = _idx_day[0]
-            next_day = True
+
+            _hist, _ = np.histogram(hours[:idx_day+1], bins=bins)
+            # Add leftover from previous day if there is any
+            # If the end of leftover is later than the beginning of
+            # the current dataset, then they are not in the same day
+            # so add separately
+            if _hours_tmp is not None:
+                _hist_prev, _ = np.histogram(_hours_tmp, bins=bins)
+
+                if _hours_tmp[-1] > hours[0]:
+                    hist_days.append(_hist_prev)
+                else:
+                    _hist += _hist_prev
+
+            hist_days.append(_hist)
+            _hours_tmp = hours[idx_day+1:]
         else:
-            idx_day = hours.size
-
-        _hist, _ = np.histogram(hours[:idx_day+1], bins=bins)
-
-        # If there is left over hours from previous dataset, add them
-        # to the histogram
-        if hours_tmp is not None:
-            _hist_prev, _ = np.histogram(hours_tmp, bins=bins)
-            _hist += _hist_prev
-
-        hist_days.append(_hist)
-
-        if next_day:
-            hours_tmp = hours[idx_day+1:]
-        else:
-            hours_tmp = None
+            if _hours_tmp is None:
+                _hours_tmp = hours[:]
+            else:
+                _hours_tmp = np.concatenate([_hours_tmp, hours])
 
     # Add the final bit if there is leftover
-    if hours_tmp is not None:
-        _hist_leftover, _ = np.histogram(hours_tmp, bins=bins)
+    if _hours_tmp is not None:
+        _hist_leftover, _ = np.histogram(_hours_tmp, bins=bins)
         hist_days.append(_hist_leftover)
 
     return np.asarray(hist_days)
@@ -182,6 +188,9 @@ def get_livetime_window(sphere, dataset, data_prefix, nfile):
     data_dir = fr'E:\dm_data_processed_amp_chisquare\{sphere}\{dataset}'
 
     good_window_times = np.empty(6000 * nfile)
+    # timestamps_file = np.empty(nfile)
+    # window_count = np.empty(nfile)
+
     window_count = 0
     for i in range(nfile):
         if i % 100 == 0:
@@ -200,17 +209,82 @@ def get_livetime_window(sphere, dataset, data_prefix, nfile):
         _good_window_time = timestamp + dt_window * _good_window_idx
         good_window_times[window_count: window_count+_good_window_time.size] = _good_window_time
 
-        window_count += _good_window_time.size
+        window_count += 1
 
+    # Return up to the windows that pass livetime selection
     return good_window_times[:window_count]
 
-for i, dataset in enumerate(datasets):
+def get_hist_per_dataset(sphere, datasets, bins, sidereal=True):
+    print(sphere, sidereal)
+
+    prefix_hist = 'sidereal' if sidereal else 'solar'
+
     out_dir = rf'E:\livetime_window\{sphere}'
-    if not os.path.isdir(out_dir):
-            os.mkdir(out_dir)
 
-    outfile = rf'{sphere}_{dataset}_livetime_window.npz'
-    good_window_times = get_livetime_window(sphere, dataset, data_prefixs[i], n_files[i])
+    for dataset in datasets:
+        outfile = rf'{sphere}_{dataset}_hist_livetime_window_{prefix_hist}.npz'
+        # if( os.path.isfile(os.path.join(out_dir, outfile)) ):
+        #     print("Skipping: ", outfile)
+        #     continue
 
-    print(f'Writing file {os.path.join(out_dir, outfile)}')
-    np.savez(os.path.join(out_dir, outfile), livetime_window=good_window_times)
+        npz = np.load(fr"E:\livetime_window\{sphere}\{sphere}_{dataset}_livetime_window.npz")
+        time_window = npz['livetime_window']
+        npz.close()
+
+        tt = Time(time_window, format='unix', scale='utc')
+        if sidereal:
+            lst = tt.sidereal_time('apparent', longitude=location.lon)
+            hours = lst.hour
+        else:
+            utc_offset = -5
+            hours = get_local_hour(utc_offset, tt)
+
+        # Compute histogram until the end of day
+        _idx_day = np.where(np.diff(hours) < 0)[0]
+
+        # If there is an end of day in the dataset
+        # Bin into histogram until the end of day
+        # and store the rest as tmp
+        if _idx_day.size == 2:
+            _hist_0, _ = np.histogram(hours[ : _idx_day[0]+1], bins=bins)
+            _hist_1, _ = np.histogram(hours[_idx_day[0]+1 : _idx_day[1]+1], bins=bins)
+            _hist_2, _ = np.histogram(hours[_idx_day[1]+1 : ], bins=bins)
+        elif _idx_day.size == 1:
+            idx_day = _idx_day[0]
+            _hist_0, _ = np.histogram(hours[:idx_day+1], bins=bins)
+            _hist_1, _ = np.histogram(hours[idx_day+1:], bins=bins)
+            _hist_2 = None
+        else:
+            _hist_0, _ = np.histogram(hours, bins=bins)
+            _hist_1 = None
+            _hist_2 = None
+
+        print(f'Writing file {os.path.join(out_dir, outfile)}')
+        np.savez(os.path.join(out_dir, outfile), hist_0=_hist_0, hist_1=_hist_1, hist_2=_hist_2)
+
+if __name__ == "__main__":
+    sphere_idx    = int(sys.argv[1])
+    time_type     = sys.argv[2]
+    
+    if sphere_idx == 0:
+        sphere = sphere_0
+        datasets = datasets_0
+    elif sphere_idx == 1:
+        sphere = sphere_1
+        datasets = datasets_1
+    
+    sidereal = True if time_type == 'sidereal' else False
+
+    bins = np.linspace(0, 24, 25)
+    get_hist_per_dataset(sphere, datasets, bins, sidereal=sidereal)
+
+# for i, dataset in enumerate(datasets):
+#     out_dir = rf'E:\livetime_window\{sphere}'
+#     if not os.path.isdir(out_dir):
+#             os.mkdir(out_dir)
+
+#     outfile = rf'{sphere}_{dataset}_livetime_window.npz'
+#     good_window_times = get_livetime_window(sphere, dataset, data_prefixs[i], n_files[i])
+
+#     print(f'Writing file {os.path.join(out_dir, outfile)}')
+#     np.savez(os.path.join(out_dir, outfile), livetime_window=good_window_times)
