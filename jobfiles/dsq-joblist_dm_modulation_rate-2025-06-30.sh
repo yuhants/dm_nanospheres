d54dc18821003b12003fb33f6489477205f3399e
Yu-Han Tseng <yt388@r904u07n02.grace.ycrc.yale.edu>
2025-06-30 14:22:17 -0400

    full daily modulation calc

--- jobfiles/dsq-joblist_dm_modulation_rate-2025-06-30.sh
+++ jobfiles/dsq-joblist_dm_modulation_rate-2025-06-30.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --output dsq-joblist_dm_modulation_rate-%A_%4a-%N.out
-#SBATCH --array 0-4399
+#SBATCH --output dsq-joblist_dm_modulation_rate-%A_%3a-%N.out
+#SBATCH --array 0-941
 #SBATCH --job-name dsq-joblist_dm_modulation_rate
 #SBATCH --mem=4G -t 00:15:00 --mail-type=ALL --mail-user=yuhan.tseng@yale.edu --partition=scavenge --requeue
 
--- jobfiles/make_joblist_modulation_rate.py
+++ jobfiles/make_joblist_modulation_rate.py
@@ -27,9 +27,16 @@ def get_sidereal_bins(t0, lst_hour0, ndays):
 
     return np.asarray(bins_sidereal_timestamps) + correction
 
+data_dir = '/home/yt388/microspheres/dm_nanospheres/data_processed/dm_rate/daily_modulation'
+npz = np.load(f'{data_dir}/dsdqdv_nanosphere_5.29278e-01_1.23616e-06_1e+00.npz')
+# npz = np.load(f'{data_dir}/dsdqdv_nanosphere_1.77828e+00_2.09209e-07_1e+00.npz')
+# npz = np.load(f'{data_dir}/dsdqdv_nanosphere_4.68870e+03_1.74604e-05_1e+00.npz')
+mx = npz['mx_gev'] * 1e9
+alpha_n = npz['alpha_n']
+m_phi = 1
+
 bins_sidereal_1 = get_sidereal_bins(time1[0], Time(time1[0], format='unix', scale='utc').sidereal_time('apparent', longitude=longitude_newhaven).hour, 22)
 bins_siderealdays_1 = np.array([bins_sidereal_1[i] for i in range(bins_sidereal_1.size) if i % 24 == 0])
-
 
 outfile = 'joblist_dm_modulation_rate.txt'
 
@@ -39,6 +46,13 @@ for i in range(bins_siderealdays_1.size - 1):
     timestamps = np.linspace(bins_siderealdays_1[i], bins_siderealdays_1[i+1], 200, endpoint=False)
 
     for t in timestamps:
+        outdir = r'/home/yt388/palmer_scratch/data/dm_rate/daily_modulation'
+        file_name = f'/drdqz_{mx:.5e}_{alpha_n:.5e}_{m_phi:.0e}_{int(t)}.npz'
+
+        if( os.path.isfile(outdir+file_name) ):
+            print("Skipping: ", file_name)
+            continue
+
         job_str = f'module load miniconda; conda activate microsphere; python ../dm_modulation_rate.py {t}\n'
         job_file.write( job_str )
 
